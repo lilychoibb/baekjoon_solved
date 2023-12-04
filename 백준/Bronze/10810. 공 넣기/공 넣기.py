9cb93da04fa194287ec84430685978dd6ec91592
yoonseo choi <choiyoonseo02@gmail.com>
2023-12-04 16:04:20 +0900

    [Bronze III] Title: 공 넣기, Time: 52 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+
+basket = [0 for _ in range(n)]
+
+for _ in range(m):
+    i, j, k = map(int, input().split())
+    for a in range(i, j+1):
+        basket[a-1] = k
+        
+for a in range(n):
+    print(basket[a], end = ' ')
