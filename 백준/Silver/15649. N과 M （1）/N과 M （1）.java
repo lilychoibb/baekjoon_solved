5b4205f28dc36991b3b6fe440d923d64f4c05e8d
yoonseo choi <choiyoonseo02@gmail.com>
2024-07-11 11:43:13 +0900

    [Silver III] Title: N과 M (1), Time: 1864 ms, Memory: 67508 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] arr;
+    static boolean[] visit;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        arr = new int[m];
+        visit = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            visit[i] = false;
+        }
+        dfs(n, m, 0);
+    }
+
+    public static void dfs(int n, int m, int depth) {
+        if (depth == m) {
+            for (int value : arr) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(n, m, depth+1);
+                visit[i] = false;
+            }
+        }
+    }
+}
