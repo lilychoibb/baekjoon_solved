d2f4dc7d4906471ea960141c3ba5a96c1f86bf26
yoonseo choi <choiyoonseo02@gmail.com>
2024-08-17 19:49:56 +0900

    [Bronze III] Title: 택시 기하학, Time: 172 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // ㅠr^2
+        // 2r^2
+
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+
+        System.out.printf("%.6f%n", i * i * Math.PI);
+        System.out.printf("%.6f%n", i * i * 2.000000);
+    }
+}
