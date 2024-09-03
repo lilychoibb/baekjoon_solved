49249886b5bce5bf8b4320d40832169d60f716f8
yoonseo choi <choiyoonseo02@gmail.com>
2024-09-03 23:19:02 +0900

    [Bronze I] Title: 2진수 8진수, Time: 500 ms, Memory: 33972 KB -BaekjoonHub

---
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder s = new StringBuilder(sc.nextLine());
+
+        int j = 0;
+        if (s.length() % 3 != 0) {
+            j = 3 - (s.length() % 3);
+        }
+
+        for (int i = 0; i < j; i++) {
+            s.insert(0, "0");
+        }
+
+        StringBuilder sum = new StringBuilder();
+        for (int i = 0; i < s.length(); i += 3) {
+            sum.append((Character.getNumericValue(s.charAt(i)) * 4) +
+                       (Character.getNumericValue(s.charAt(i + 1)) * 2) +
+                       (Character.getNumericValue(s.charAt(i + 2))));
+        }
+
+        System.out.println(sum);
+    }
+}
