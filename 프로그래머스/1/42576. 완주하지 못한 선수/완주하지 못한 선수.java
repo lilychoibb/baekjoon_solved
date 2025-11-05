b503a1a3cb80a69ff33a3e1fc84b1f1e5d316af4
yoonseo choi <choiyoonseo02@gmail.com>
2025-11-06 02:09:04 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 99.10 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,28 +2,20 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        HashMap<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
+
         for (String p : participant) {
-            if (!map.containsKey(p)) {
-                map.put(p, 1);
-            } else {
-                map.put(p, map.get(p) + 1);
-            }
+            map.put(p, map.getOrDefault(p, 0) + 1);
         }
 
-        for (String pp : completion) {
-            if (map.containsKey(pp) && map.get(pp) == 1) {
-                map.remove(pp);
-            } else {
-                map.put(pp, map.get(pp) - 1);
-            }
+        for (String c : completion) {
+            map.put(c, map.get(c) - 1);
         }
-        HashSet<String> set = new HashSet<>(map.keySet());
 
-        for (String s : set) {
-            answer = s;
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) return key;
         }
-        return answer;
+
+        return "";
     }
 }
