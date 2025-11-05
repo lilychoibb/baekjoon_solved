53237d1a3ee6c09fb33118b09a7da98a3eb7c23d
yoonseo choi <choiyoonseo02@gmail.com>
2025-11-06 02:13:19 +0900

    [level 1] Title: K번째수, Time: 1.11 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,19 +1,21 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
-        List<Integer> answerTemp = new ArrayList<>();
-        for (int[] i : commands) {
-            int start = i[0] - 1;
-            int finish = i[1];
-            List<Integer> list = new ArrayList<>();
-            for (int j = start; j < finish; j++) {
-                list.add(array[j]);
-            }
-            list.sort(Comparator.comparingInt(a -> a));
-            answerTemp.add(list.get(i[2] - 1));
+
+        int[] answer = new int[commands.length];
+
+        for (int idx = 0; idx < commands.length; idx++) {
+            int i = commands[idx][0];
+            int j = commands[idx][1];
+            int k = commands[idx][2];
+
+            int[] temp = Arrays.copyOfRange(array, i-1, j);
+            Arrays.sort(temp);
+
+            answer[idx] = temp[k-1];
         }
-        answer = answerTemp.stream().mapToInt(i -> i).toArray();
+
         return answer;
     }
 }
